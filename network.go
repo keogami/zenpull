d5656bcb3176016f989215715d91cca204c42396
keogami <41939011+keogami@users.noreply.github.com>
2021-12-29 02:42:08 +0530

    added some helping comments

--- network.go
+++ network.go
@@ -34,15 +34,15 @@ func netCopy(fromUrl, toFile string) error {
 
 func rangedDownload(w io.Writer, fromUrl string, length int64) error {
 	downloaded := int64(0)
-	client := &http.Client{}
-	for downloaded != length {
+	client := &http.Client{}   // we dont need to create a client
+	for downloaded != length { // maybe the infinite download bug is caused by this equality test
 		req, err := http.NewRequest(http.MethodGet, fromUrl, nil)
 		if err != nil {
 			return err
 		}
 		req.Header.Set("Range", fmt.Sprintf("bytes=%d-", downloaded))
 
-		resp, err := client.Do(req)
+		resp, err := client.Do(req) // use http.DO instead
 		if err != nil {
 			return err
 		}
@@ -52,7 +52,7 @@ func rangedDownload(w io.Writer, fromUrl string, length int64) error {
 			fmt.Println("err:", err, "; more:", more) // for debugging, in case i see it again
 		}
 		downloaded += more
-		resp.Body.Close()
+		resp.Body.Close() // use defer instead of closing it here
 	}
 	return nil
 }
