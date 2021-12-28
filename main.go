5f2b637b9fc8c4540afed509252bb847de1b2ebc
keogami <41939011+keogami@users.noreply.github.com>
2021-12-29 02:20:56 +0530

    removed the redundant Meta structure

--- main.go
+++ main.go
@@ -4,62 +4,18 @@ import (
 	"bufio"
 	"crypto/sha1"
 	"encoding/hex"
-	"encoding/json"
 	"fmt"
 	"io"
 	"os"
 	"path"
 	"path/filepath"
-	"strconv"
 	"sync"
-	"time"
 )
 
 var (
 	workerCount           = 5
 	directoryExistenceMap = make(map[string]bool)
 )
-
-// Meta is the meta information to be persisted, required by the organiser
-// [note] this is no longer needed, so it should be removed
-type Meta struct {
-	Base string `json:"base"`
-}
-
-func (m Meta) store(path string) error {
-	file, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	err = json.NewEncoder(file).Encode(m)
-	return err
-}
-
-func makeMeta() Meta {
-	t := time.Now().Unix()
-	base := strconv.FormatInt(t, 16)
-	return Meta{
-		Base: base,
-	}
-}
-
-func getMeta() (Meta, error) {
-	cfile, err := os.Open("meta.json")
-	if err != nil {
-		return Meta{}, err
-	}
-	defer cfile.Close()
-
-	var meta Meta
-	err = json.NewDecoder(cfile).Decode(&meta)
-	if err != nil {
-		return Meta{}, err
-	}
-
-	return meta, nil
-}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -77,43 +33,28 @@ func main() {
 	}
 	defer file.Close()
 
-	meta, err := getMeta()
-	if err != nil {
-		fmt.Println("couldn't load meta from meta.json")
-		fmt.Println("error:", err)
-		fmt.Println("just creating a new one :p")
-		meta = makeMeta()
-	}
-
-	mkdirIfRequired(meta.Base)
-
-	tap := dispatch(file, meta)
+	tap := dispatch(file)
 
 	wg := new(sync.WaitGroup)
 	wg.Add(workerCount)
 
 	for i := 0; i < workerCount; i++ {
-		go retrieve(wg, tap, meta)
+		go retrieve(wg, tap)
 	}
 
 	wg.Wait()
 
-	err = meta.store("meta.json")
-	if err != nil {
-		fmt.Println("couldn't store meta at meta.json, bummer TwT")
-	}
-
 	fmt.Println("done <3")
 }
 
-func dispatch(from io.Reader, meta Meta) <-chan string {
+func dispatch(from io.Reader) <-chan string {
 	out := make(chan string, workerCount)
 	scanner := bufio.NewScanner(from)
 	go func(s *bufio.Scanner, o chan<- string) {
 		for s.Scan() {
 			url := s.Text()
 			ext := string(path.Ext(url)[1:])
-			mkdirIfRequired(filepath.Join(meta.Base, ext))
+			mkdirIfRequired(filepath.Join("./", ext))
 			o <- url
 		}
 		close(out)
@@ -142,12 +83,11 @@ func mkdirIfRequired(p string) {
 	directoryExistenceMap[p] = true
 }
 
-func retrieve(wg *sync.WaitGroup, tap <-chan string, meta Meta) {
+func retrieve(wg *sync.WaitGroup, tap <-chan string) {
 	defer wg.Done()
-	base := meta.Base
 
 	for url := range tap {
-		dest := makeDestPath(url, base)
+		dest := makeDestPath(url)
 		_, err := os.Stat(dest)
 		if err == nil {
 			fmt.Printf("skipping: %s\n", url)
@@ -161,10 +101,10 @@ func retrieve(wg *sync.WaitGroup, tap <-chan string, meta Meta) {
 	}
 }
 
-func makeDestPath(url, base string) string {
+func makeDestPath(url string) string {
 	ext := string(path.Ext(url)[1:])
 	hash := sha1.Sum([]byte(url))
 	hashText := hex.EncodeToString(hash[:])
-	dest := filepath.Join(base, ext, hashText) + "." + ext
+	dest := filepath.Join("./", ext, hashText) + "." + ext
 	return dest
 }
